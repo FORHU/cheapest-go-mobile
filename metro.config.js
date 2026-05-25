5f28b53c5312d9c2a975fc5dfb1a88d2a0e6458d
maegju <clydeantonio.work@gmail.com>
2026-05-25 11:18:15 +0800

    refactor: remove workspace dependencies and external module paths from metro configuration and tsconfig

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const path = require("path");
 
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, "..");
-
-const config = getDefaultConfig(projectRoot);
-
-// Add the web project to watch folders so we can import code from it
-config.watchFolders = [path.resolve(workspaceRoot, "cheapest-go-app")];
+const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, { input: "./global.css" });
